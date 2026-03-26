096dd8532f8b0038b6d1911486c55d5efb31dffb
Анна Гурьева <anna.gureva@travelata.ru>
2026-03-26 15:48:22 +0300

    remove build from repo

--- build/CMakeFiles/tests.dir/tests_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/tests_autogen/mocs_compilation.cpp.o: \
- /home/anya/Desktop/cadproj/build/tests_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anya/Desktop/cadproj
-# Build directory: /home/anya/Desktop/cadproj/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
